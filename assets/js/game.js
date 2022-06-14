3ddc3aed3f1be9a189e6ffe6a3f7150828e60539
Busatron <bussea38@gmail.com>
2022-06-13 22:03:01 -0500

    Added start and end game

--- assets/js/game.js
+++ assets/js/game.js
@@ -82,15 +82,43 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   }
 }
 
-for(var i = 0; i < enemyNames.length; i++) {
+var startGame = function () {
+
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+
+  for(var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators!" + ( i +1 ) );
+      var pickedEnemyName = enemyNames[i]
+      fight(pickedEnemyName);
+      enemyHealth = 50;
+    }
+    else {
+      window.alert("You have lost your robot in battle! Game over!")
+      break;
+    }
+  }
+    endGame();
+}
+var endGame = function () {
   if (playerHealth > 0) {
-    window.alert("Welcome to Robot Gladiators!" + ( i +1 ) );
-    var pickedEnemyName = enemyNames[i]
-    fight(pickedEnemyName);
-    enemyHealth = 50;
+    window.alert("Greath job, you've survived the game! You now have a score of " + playerMoney + ".");
   }
   else {
-    window.alert("You have lost your robot in battle! Game over!")
-    break;
+    window.alert("You've lost your robot in battle.");
   }
+};
+
+var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  startGame();
 }
+
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!")
+}
+
+startGame();
